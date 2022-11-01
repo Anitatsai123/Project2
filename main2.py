3c18cea3692c482f97f4ecbbbe6d74296be36cb3
Anita Tsai <anitatsai123@gmail.com>
2022-11-01 11:19:08 +0800

    modify

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import tkinter as tk
 #建立class取代Window類別內個別方法設定, 繼承父類別tk.Canvas, 修改視窗內之按鈕
 class ColorCanvas(tk.Canvas):
     #建立Class的property
-    #建立一個Class內建的常數
+    #建立一個Class內建的常數(=不可變更)
     ON =  True
     OFF = False
     def __init__(self,parent,rec_color,**kwargs):
@@ -11,9 +11,7 @@ class ColorCanvas(tk.Canvas):
         super().__init__(parent,**kwargs)
         self.rec_color = rec_color
         self.__state = ColorCanvas.OFF
-        self.space = self.width / 7
-        rec_width = self.width  - 2 * self.space 
-        rec_height = self.height - 2 * self.space   
+        self.space = self.width / 7  
         self.create_rectangle(self.space, self.space, self.width - self.space, self.height - self.space,fill=self.rec_color)
 
     
@@ -28,7 +26,16 @@ class ColorCanvas(tk.Canvas):
         if self.__state == True:
             #多加小圓點
             print("多加小圓點")
-            
+            rec_width = self.width  - 2 * self.space 
+            rec_height = self.height - 2 * self.space
+
+            cir_width = rec_width / 5
+            cir_height = rec_height / 5
+            cir_start_x = self.space + cir_width / 2
+            cir_end_x = cir_start_x + cir_width
+            cir_start_y = self.space + rec_height * 5 / 7
+            cir_end_y  =  cir_start_y + cir_height
+            self.create_oval(cir_start_x,cir_start_y,cir_end_x,cir_end_y,fill='white',outline='white')
 
        
 
@@ -41,11 +48,13 @@ class Window(tk.Tk):
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
         red.state = ColorCanvas.ON
-        print(f"red狀態:{red.state}")
+        
         green = ColorCanvas(self,"green",width=100,height=100)        
         green.grid(row=0, column=1)
+        green.state = ColorCanvas.ON
         blue = ColorCanvas(self,"blue",width=100,height=100)        
         blue.grid(row=0, column=2)
+        blue.state = ColorCanvas.ON
 
     
     def mouse_click(self, event):
